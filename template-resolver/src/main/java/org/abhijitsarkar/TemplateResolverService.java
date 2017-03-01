582ad89a383ff30886b2041d5649f5ed42e1964e
Abhijit Sarkar <asarkar@users.noreply.github.com>
2017-03-01 01:10:31 -0800

    Update TemplateResolverService.java

--- template-resolver/src/main/java/org/abhijitsarkar/TemplateResolverService.java
+++ template-resolver/src/main/java/org/abhijitsarkar/TemplateResolverService.java
@@ -1,7 +1,7 @@
 package org.abhijitsarkar;
 
-import lombok.RequiredArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.context.Context;
